cf1d6d0520e40588d6d637d3294d0e9a03e2aca1
Alex Osler <alexpetero98@gmail.com>
2018-05-04 00:25:11 -0400

    work dangit

--- scripts/main.js
+++ scripts/main.js
@@ -6,8 +6,6 @@ document.getElementById("dimensions").onkeypress = function (event) {
     }
     let keyPressed = event.keyPressed || event.which;
     //test
-    //test2
-    //test3
     if (keyPressed === 13) {
         let dimensions = event.target.value;
         dimensions = Number(dimensions);
